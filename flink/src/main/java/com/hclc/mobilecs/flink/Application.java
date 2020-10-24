0ca68b4b1e8870627cab52d6acdd1e9455898977
Bartosz Kaminski <bartosz.kaminski@zoho.com>
2020-10-24 23:26:37 +0200

    Kafka producer attached to basic Flink application

--- flink/src/main/java/com/hclc/mobilecs/flink/Application.java
+++ flink/src/main/java/com/hclc/mobilecs/flink/Application.java
@@ -1,10 +1,15 @@
 package com.hclc.mobilecs.flink;
 
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 
-import static java.time.Instant.parse;
+import java.util.Properties;
+
+import static java.time.ZonedDateTime.parse;
 import static java.util.Date.from;
 
 public class Application {
@@ -12,18 +17,31 @@ public class Application {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<IncomingDataRecord> incomingDataRecords = env.fromElements(
-                new IncomingDataRecord(from(parse("2020-01-01T01:00:00+01:00[Europe/Warsaw]")), "4841342423", 8011),
-                new IncomingDataRecord(from(parse("2020-01-01T02:00:00+01:00[Europe/Warsaw]")), "4841342423", 7002112),
-                new IncomingDataRecord(from(parse("2020-01-01T03:00:00+01:00[Europe/Warsaw]")), "4841342423", 90112),
-                new IncomingDataRecord(from(parse("2020-01-01T04:00:00+01:00[Europe/Warsaw]")), "4841342423", 2002112)
+                new IncomingDataRecord(from(parse("2020-01-01T01:00:00+01:00[Europe/Warsaw]").toInstant()), "4841342423", 8011),
+                new IncomingDataRecord(from(parse("2020-01-01T02:00:00+01:00[Europe/Warsaw]").toInstant()), "4841342423", 7002112),
+                new IncomingDataRecord(from(parse("2020-01-01T03:00:00+01:00[Europe/Warsaw]").toInstant()), "4841342423", 90112),
+                new IncomingDataRecord(from(parse("2020-01-01T04:00:00+01:00[Europe/Warsaw]").toInstant()), "4841342423", 2002112)
         );
 
-        DataStream<IncomingDataRecord> highUsage = incomingDataRecords.filter(new FilterFunction<IncomingDataRecord>() {
+        DataStream<String> highUsage = incomingDataRecords.filter(new FilterFunction<IncomingDataRecord>() {
             @Override
             public boolean filter(IncomingDataRecord incomingDataRecord) throws Exception {
                 return incomingDataRecord.getRecordedBytes() >= 1024 * 1024;
             }
+        }).map(new MapFunction<IncomingDataRecord, String>() {
+            @Override
+            public String map(IncomingDataRecord incomingDataRecord) throws Exception {
+                return incomingDataRecord.toString();
+            }
         });
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        FlinkKafkaProducer<String> kafkaProducer = new FlinkKafkaProducer<>(
+                "incoming-data-records",
+                new SimpleStringSchema(),
+                properties);
+        highUsage.addSink(kafkaProducer);
 
         highUsage.print();
 
