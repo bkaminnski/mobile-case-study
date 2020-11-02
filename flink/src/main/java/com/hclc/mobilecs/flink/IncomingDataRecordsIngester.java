47dde36dec464cc47fb9171fba4b0c10142f0370
Bartosz Kaminski <bartosz.kaminski@zoho.com>
2020-11-02 22:54:30 +0100

    Minor refactoring

--- flink/src/main/java/com/hclc/mobilecs/flink/IncomingDataRecordsIngester.java
+++ flink/src/main/java/com/hclc/mobilecs/flink/IncomingDataRecordsIngester.java
@@ -1,8 +1,7 @@
 package com.hclc.mobilecs.flink;
 
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -11,50 +10,15 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
 
 import java.util.Properties;
+import java.util.UUID;
 
 public class IncomingDataRecordsIngester {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("group.id", "ingester");
-
-        FlinkKafkaConsumer<ObjectNode> agreementsKafkaConsumer = new FlinkKafkaConsumer<>(
-                "agreements",
-                new JSONKeyValueDeserializationSchema(true),
-                properties);
-        KeyedStream<Agreement, String> agreementsStream = env.addSource(agreementsKafkaConsumer)
-                .map(new MapFunction<ObjectNode, Agreement>() {
-                    @Override
-                    public Agreement map(ObjectNode value) throws Exception {
-                        return Agreement.fromJson(value);
-                    }
-                }).keyBy(new KeySelector<Agreement, String>() {
-                    @Override
-                    public String getKey(Agreement value) throws Exception {
-                        return value.getMsisdn();
-                    }
-                });
-
-        FlinkKafkaConsumer<ObjectNode> incomingDataRecordsKafkaConsumer = new FlinkKafkaConsumer<>(
-                "incoming-data-records",
-                new JSONKeyValueDeserializationSchema(true),
-                properties);
-        KeyedStream<EnrichedIncomingDataRecord, String> incomingDataRecordsStream = env.addSource(incomingDataRecordsKafkaConsumer)
-                .map(new MapFunction<ObjectNode, EnrichedIncomingDataRecord>() {
-                    @Override
-                    public EnrichedIncomingDataRecord map(ObjectNode value) throws Exception {
-                        return EnrichedIncomingDataRecord.fromJson(value);
-                    }
-                }).keyBy(new KeySelector<EnrichedIncomingDataRecord, String>() {
-                    @Override
-                    public String getKey(EnrichedIncomingDataRecord value) throws Exception {
-                        return value.getMsisdn();
-                    }
-                });
-
-        SingleOutputStreamOperator<DataRecord> dataRecordsStream = agreementsStream.connect(incomingDataRecordsStream).flatMap(new MatchingFunction());
+        SingleOutputStreamOperator<DataRecord> dataRecordsStream = agreementsStream(env)
+                .connect(incomingDataRecordsStream(env))
+                .flatMap(new MatchingFunction());
 
         CassandraSink.addSink(dataRecordsStream)
                 .setHost("127.0.0.1")
@@ -63,4 +27,55 @@ public class IncomingDataRecordsIngester {
         env.execute();
     }
 
+    private static KeyedStream<Agreement, String> agreementsStream(StreamExecutionEnvironment env) {
+        return readingAgreementsFromKafka(env)
+                .map(IncomingDataRecordsIngester::agreementFromJson)
+                .keyBy(IncomingDataRecordsIngester::msisdnInAgreement);
+    }
+
+    private static DataStreamSource<ObjectNode> readingAgreementsFromKafka(StreamExecutionEnvironment env) {
+        FlinkKafkaConsumer<ObjectNode> agreementsKafkaConsumer = new FlinkKafkaConsumer<>(
+                "agreements",
+                new JSONKeyValueDeserializationSchema(true),
+                kafkaProperties()
+        );
+        return env.addSource(agreementsKafkaConsumer);
+    }
+
+    private static Agreement agreementFromJson(ObjectNode value) throws Exception {
+        return Agreement.fromJson(value);
+    }
+
+    private static String msisdnInAgreement(Agreement value) throws Exception {
+        return value.getMsisdn();
+    }
+
+    private static KeyedStream<EnrichedIncomingDataRecord, String> incomingDataRecordsStream(StreamExecutionEnvironment env) {
+        return readingIncomingDataRecordsFromKafka(env)
+                .map(IncomingDataRecordsIngester::incomingDataRecordFromJson)
+                .keyBy(IncomingDataRecordsIngester::msisdnInIncomingDataRecord);
+    }
+
+    private static DataStreamSource<ObjectNode> readingIncomingDataRecordsFromKafka(StreamExecutionEnvironment env) {
+        FlinkKafkaConsumer<ObjectNode> incomingDataRecordsKafkaConsumer = new FlinkKafkaConsumer<>(
+                "incoming-data-records",
+                new JSONKeyValueDeserializationSchema(true),
+                kafkaProperties());
+        return env.addSource(incomingDataRecordsKafkaConsumer);
+    }
+
+    private static EnrichedIncomingDataRecord incomingDataRecordFromJson(ObjectNode value) throws Exception {
+        return EnrichedIncomingDataRecord.fromJson(value);
+    }
+
+    private static String msisdnInIncomingDataRecord(EnrichedIncomingDataRecord value) throws Exception {
+        return value.getMsisdn();
+    }
+
+    private static Properties kafkaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "ingester" + UUID.randomUUID().toString());
+        return properties;
+    }
 }
